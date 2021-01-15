665b1fbe1d7909b3bc10faf9f5994288d7001a50
ruddy palvair <r.palvair@gmail.com>
2021-01-15 16:41:38 +0100

    feat : Experiment new methods for the nested classes

---
+++ src/main/java/com/palvair/Outer.java
@@ -0,0 +1,8 @@
+package com.palvair;
+
+public class Outer {
+
+    public class Nested {
+
+    }
+}
---
+++ src/test/java/com/palvair/NestedMatesImprovementsTest.java
@@ -0,0 +1,32 @@
+package com.palvair;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NestedMatesImprovementsTest {
+
+    @Test
+    public void should_return_true_when_nested_classes() {
+        final Outer outer = new Outer();
+        final Outer.Nested nested = outer.new Nested();
+
+        assertThat(outer.getClass().isNestmateOf(nested.getClass())).isTrue();
+    }
+
+    @Test
+    public void should_return_name_of_outer_class_when_calling_get_nest_host_on_nested_class() {
+        final Outer outer = new Outer();
+        final Outer.Nested nested = outer.new Nested();
+
+        assertThat(nested.getClass().getNestHost()).isEqualTo(Outer.class);
+    }
+
+    @Test
+    public void should_return_name_of_outer_class_and_nested_class_when_calling_get_nest_members_on_nested_class() {
+        final Outer.Nested nested = new Outer().new Nested();
+
+        assertThat(nested.getClass().getNestMembers())
+                .containsExactly(Outer.class, Outer.Nested.class);
+    }
+}
